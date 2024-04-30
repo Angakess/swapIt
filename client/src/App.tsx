d550a3a381a78df27a640dce217ea3774448cc77
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-04-30 01:26:46 -0300

    add ant theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthRoutes } from '@Auth/routes'
+import { ConfigProvider } from 'antd'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth/*" element={<AuthRoutes />} />
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#14518b',
+          colorInfo: '#d02f4c',
+          colorTextBase: '#061726',
+        },
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth/*" element={<AuthRoutes />} />
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   )
 }
 
