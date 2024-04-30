39f087001509d75d05c4cea602e0fb2fe8bf4a2d
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-04-30 09:09:15 -0300

    add <AuthLayout /> and public/logo-caritas.svg

--- client/src/features/auth/layouts/AuthLayout.tsx
+++ client/src/features/auth/layouts/AuthLayout.tsx
@@ -1,8 +1,27 @@
+import { Card, Col, Flex, Layout, Row } from 'antd'
+
 export function AuthLayout({ children }: React.PropsWithChildren) {
   return (
-    <div>
-      <h1>AuthLayout</h1>
-      {children}
-    </div>
+    <Layout>
+      <Layout.Content>
+        <Row justify="center" align="top" style={{ minHeight: '100dvh' }}>
+          <Col xs={22} style={{ maxWidth: '400px', marginTop: '10dvh' }}>
+            <Flex justify="center" align="center">
+              <img
+                src="/logo-caritas.svg"
+                alt="logo caritas"
+                style={{
+                  height: '5rem',
+                  marginBottom: '1.25rem',
+                }}
+              />
+            </Flex>
+            <Card bordered={false} styles={{ body: { padding: '2rem' } }}>
+              {children}
+            </Card>
+          </Col>
+        </Row>
+      </Layout.Content>
+    </Layout>
   )
 }
