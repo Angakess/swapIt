e07d59b86165447964bf96958e98583cb3335d7b
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-01 20:18:12 -0300

    add default redirect to /auth

--- client/src/features/auth/routes/index.tsx
+++ client/src/features/auth/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { AuthLayout } from '@Auth/layouts'
 import { ForgotPassword, Login, Register } from '@Auth/pages'
 
@@ -9,6 +9,7 @@ export function AuthRoutes() {
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
         <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="/*" element={<Navigate to="/auth/login" />} />
       </Routes>
     </AuthLayout>
   )
