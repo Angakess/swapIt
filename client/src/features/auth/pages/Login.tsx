d205ee3ead2f2f1373dd6423a02e29fa19da8384
Andres <andresgabriel.kessler@gmail.com>
2024-05-04 17:39:27 -0300

    se agrego logica basica para validar en Login

--- client/src/features/auth/pages/Login.tsx
+++ client/src/features/auth/pages/Login.tsx
@@ -1,17 +1,41 @@
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons'
-import { Button, Flex, Form, Input, InputNumber, Typography } from 'antd'
+import { Button, Flex, Form, Input, InputNumber, Typography, FormProps } from 'antd'
 
 import { AuthTitle } from '@Auth/components'
 import { Link } from 'react-router-dom'
 
 export function Login() {
+ 
+  const onFinish: FormProps<number | string | null>['onFinish'] = (values) => {
+    console.log("Success: ", values)
+  };
+  const onFinishFailed: FormProps<number | string | null>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed: ', errorInfo)
+  };
+ 
+  
   return (
     <>
       <AuthTitle>Iniciar sesión</AuthTitle>
-      <Form layout="vertical">
-        <Form.Item label="DNI">
+      <Form
+        layout="vertical"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item 
+          label="DNI"
+          name="dni"
+          required={false}
+          rules={[{required: true, message: "Porfavor ingrese su DNI"},
+                  {validator: (_, value) => 
+                    value && value.toString().length === 8  ?
+                      Promise.resolve() :
+                      Promise.reject("El DNI debe ser un número de 8 dígitos")
+                  },
+          ]}    
+          >
           <InputNumber
-            placeholder="DNI"
+            placeholder="Ingrese su DNI"
             size="large"
             style={{ width: '100%' }}
             controls={false}
@@ -19,9 +43,14 @@ export function Login() {
           />
         </Form.Item>
 
-        <Form.Item label="Contraseña" style={{ marginBottom: '0.25rem' }}>
+        <Form.Item 
+          label="Contraseña"
+          name="password"
+          required={false}
+          rules={[{required: true, message: "Porfavor ingrese su contraseña"}]} 
+          style={{ marginBottom: '0.25rem' }}>
           <Input.Password
-            placeholder="Contraseña"
+            placeholder="Ingrese su contraseña"
             size="large"
             iconRender={(visible) =>
               visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
@@ -40,6 +69,7 @@ export function Login() {
             block
             type="primary"
             size="large"
+            htmlType='submit'
             style={{ marginTop: '0.5rem' }}
           >
             Iniciar sesión
