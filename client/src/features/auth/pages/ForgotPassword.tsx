9ec0b2c0c5358c91371401788c8f39cfafa17e3e
Andres <andresgabriel.kessler@gmail.com>
2024-05-04 14:40:38 -0300

    se agrego logica basica para validar el DNI ingresado en ForgotPassword

--- client/src/features/auth/pages/ForgotPassword.tsx
+++ client/src/features/auth/pages/ForgotPassword.tsx
@@ -1,23 +1,50 @@
 import { AuthTitle } from "@Auth/components";
-import { Form, InputNumber, Button } from "antd"
+import { Form, InputNumber, Button, FormProps } from "antd"
 import { useState } from "react";
 
 export function ForgotPassword() {
-  
-  
+ 
+  const [dni, setDni] = useState<number | string | null>(null)
+
+  const handleChange = (newDni: number | string | null) => {
+    console.log(newDni)
+    setDni(newDni)
+  }
+ 
+  const onFinish: FormProps<number | string | null>['onFinish'] = (values) => {
+    console.log("Success: ", values)
+  };
+
+  const onFinishFailed: FormProps<number | string | null>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed: ', errorInfo)
+  };
+
   return (
     <>
       <AuthTitle>¿Has olvidado tu contraseña?</AuthTitle>
       <Form layout="vertical"
-            style={{}}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
       >
-        <Form.Item label="DNI">
+        <Form.Item
+          label="DNI"
+          name="dni"
+          rules={[{required: true, message: 'Porfavor ingrese su DNI'},
+                  {validator: (_, value) => 
+                    value && value.toString().length === 8  ?
+                      Promise.resolve() :
+                      Promise.reject("El DNI debe ser un número de 8 dígitos")
+                  },
+          ]}
+          >
           <InputNumber
-            placeholder="DNI"
+            placeholder="Ingrese su DNI (ej: 12345678)"
             size="large"
             style={{ width: '100%' }}
             controls={false}
             autoFocus
+            value={dni}
+            onChange={handleChange}
           />
         </Form.Item>
         
@@ -28,29 +55,32 @@ export function ForgotPassword() {
           <Button
             block
             type="primary"
+            htmlType="submit"
             size="large"
             style={{ marginTop: '0.5rem'}}
           >
             Enviar
           </Button>
         </Form.Item>
+        
         <Form.Item>
           <Button
-            block
-            type="default"
-            size="middle"
+            type="link"
+            size="large"
+
+            // ni idea si esto esta bien
+            href="/auth/login"
+
             style={{ display: 'block',
               margin: 'auto',
               marginTop: '0.5rem',
-              width: '40%' }}
+              }}
           >
             Volver
           </Button>
         </Form.Item>
 
       </Form>
-
-    
     </>
   )
 }
