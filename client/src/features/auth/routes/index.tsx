b002210a787b4a000300dc95546accc1c42df119
Andres <andresgabriel.kessler@gmail.com>
2024-05-05 17:00:47 -0300

    se agrego la pagina de NewPassword

--- client/src/features/auth/routes/index.tsx
+++ client/src/features/auth/routes/index.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { AuthLayout } from '@Auth/layouts'
-import { ForgotPassword, Login, Register, Verification } from '@Auth/pages'
+import { ForgotPassword, Login, NewPassword, Register, Verification } from '@Auth/pages'
 
 export function AuthRoutes() {
   return (
@@ -10,6 +10,7 @@ export function AuthRoutes() {
         <Route path="login" element={<Login />} />
         <Route path="forgot-password" element={<ForgotPassword />} />
         <Route path="verification" element={<Verification />}/>
+        <Route path="new-password" element={<NewPassword />}/> {/* seguramente temporal, averiguar para link con vencimiento */}
         <Route path="/*" element={<Navigate to="/auth/login" />} />
       </Routes>
     </AuthLayout>
