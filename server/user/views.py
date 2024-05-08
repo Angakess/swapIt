6a045edee4330f353bd60992b8ba2b7f475b0ae4
mauri-cj <mauricocella@gmail.com>
2024-05-08 14:34:21 -0300

    authentication and permissions

---
+++ server/user/views.py
@@ -0,0 +1,27 @@
+import requests
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from swap_it.permissions import IsStaff, IsExchanger, IsHelper
+
+def activation_user(request, uid, token):
+    url = 'http://localhost:8000/auth/users/activation/'
+    data = {'uid': uid, 'token': token}
+    response = requests.post(url, data=data)
+
+    if response.status_code == 204:
+        return render(request, 'activate.html')
+    else:
+        return HttpResponse('Error en la solicitud POST')
+    
+
+class ListaVista(APIView):
+    def get(self, request, format=None):
+        print('user',self.request.user)
+        data = {'lista': ['a', 'b']}
+        return Response(data, status=status.HTTP_200_OK)
+
+    permission_classes = [IsAuthenticated, IsStaff]
