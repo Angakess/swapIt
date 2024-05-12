d577f62991fd92b9d7d62af29504606775b1e359
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-12 20:28:50 -0300

    move code from App to main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { App as AntdApp, ConfigProvider } from 'antd'
+import { Routes, Route } from 'react-router-dom'
 import { AuthRoutes } from '@Auth/routes'
 import { PostsRoutes } from '@Posts/routes'
 import { AdminRoutes } from '@Admin/routes'
 import { HomePage } from '@Home/pages'
-import { AuthProvider } from '@Auth/context'
 
 function App() {
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: '#14518b',
-          colorInfo: '#14518b',
-          colorTextBase: '#061726',
-        },
-      }}
-    >
-      <AntdApp>
-        <AuthProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/auth/*" element={<AuthRoutes />} />
-              <Route path="/posts/*" element={<PostsRoutes />} />
-              <Route path="/admin/*" element={<AdminRoutes />} />
-            </Routes>
-          </BrowserRouter>
-        </AuthProvider>
-      </AntdApp>
-    </ConfigProvider>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/auth/*" element={<AuthRoutes />} />
+      <Route path="/posts/*" element={<PostsRoutes />} />
+      <Route path="/admin/*" element={<AdminRoutes />} />
+    </Routes>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from 'App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { App as AntdApp, ConfigProvider } from 'antd'
 
 import 'antd/dist/reset.css'
 
+import { AuthProvider } from '@Auth/context'
+import App from 'App'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#14518b',
+              colorInfo: '#14518b',
+              colorTextBase: '#061726',
+            },
+          }}
+        >
+          <AntdApp>
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </AntdApp>
+        </ConfigProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
