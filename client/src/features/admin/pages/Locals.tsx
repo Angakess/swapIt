c703cba0fd3e3c4318c54aba9bcf436fb48a0a91
Andres <andresgabriel.kessler@gmail.com>
2024-05-13 22:33:41 -0300

    se puede clickear el mapa cuando se quiere agregar una filial

--- client/src/features/admin/pages/Locals.tsx
+++ client/src/features/admin/pages/Locals.tsx
@@ -1,7 +1,333 @@
+import { Button, Card, Checkbox, Col, Descriptions, DescriptionsProps, Flex, Form, Input, InputNumber, Modal, Row } from "antd";
+import { CheckboxChangeEvent } from "antd/es/checkbox";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from "react-leaflet";
+import { LatLngExpression } from 'leaflet'
+
+type SubsidiaryType = {
+  id: number,
+  name: string,
+  x_coordinate: string,
+  y_coordinate: string,
+  max_helpers: number,
+  active: boolean
+}
+type PropType = {
+  title: string
+  buttonName: string
+  handleFunction: () => void
+}
+type NewSubData = {
+  name: string
+  x_coordinate: string
+  y_coordinate: string
+  max_helpers: number
+  active: boolean
+}
+type EditedDataType = {
+  id: number
+  name: string
+  max_helpers: number | null
+  active: boolean
+}
+
 export function Locals() {
+
+  const [subsData, setSubsData] = useState<SubsidiaryType[]>([
+    {
+      id: 0,
+      name: '',
+      x_coordinate: '0',
+      y_coordinate: '0',
+      max_helpers: 0,
+      active: false,
+    },
+  ])
+
+  const [idSelected, setIdSelected] = useState<number>(0)
+
+  const [isEditingModalOpen, setIsEditingModalOpen] = useState(false)
+  const [isAddingModalOpen, setIsAddingModalOpen] = useState(false)
+
+  const [newSubData, setNewSubData] = useState<NewSubData>({
+    name: "",
+    x_coordinate: "0",
+    y_coordinate: "0",
+    max_helpers: 0,
+    active: false
+  })
+  const [newEditedSubData, setNewEditedSubData] = useState<EditedDataType>({
+    id: 0,
+    name: "",
+    max_helpers: 0,
+    active: false
+  })
+
+
+  const [locationSelected, setLocationSelected] = useState<LatLngExpression>()
+
+  const items: DescriptionsProps['items'] = [
+    {
+      key: '1',
+      label: 'Nombre',
+      children: `${subsData[idSelected].name}`,
+    },
+    {
+      key: '2',
+      label: 'Cantidad máxima de ayudantes',
+      children: `${subsData[idSelected].max_helpers}`,
+    },
+    {
+      key: '3',
+      label: 'Estado',
+      children: `${subsData[idSelected].active ? "Activa" : "Inactiva"}`,
+    }
+  ]
+
+  const fetchData = async() => {
+    const res = await fetch("http://localhost:8000/subsidiary/subsidiaries/")
+    const result = await res.json()
+    setSubsData(result)
+  }
+  useEffect(() => {fetchData()},[])
+  
+  const handleMarkerClick = (index:number) => {
+    setIdSelected(index)
+    setNewEditedSubData({
+      id: index,
+      name: subsData[index].name,
+      max_helpers: subsData[index].max_helpers,
+      active: subsData[index].active
+    })
+  }
+
+  const handleAddingModal = () => {
+    setIsAddingModalOpen(true)
+  }
+  const handleOkAdding = () => {
+    console.log("se agrego")
+  }
+  const handleCancelAdding = () => {
+    setIsAddingModalOpen(false)
+  }
+
+  const handleEditingModal = () => {
+    setIsEditingModalOpen(true)
+  }
+  const handleOkEditing = () => {
+    console.log("se edito: ", newEditedSubData)
+    fetchData()
+    setIsEditingModalOpen(false)
+  }
+  const handleCancelEditing = () => {
+    setIsEditingModalOpen(false)
+  }
+
+
+  const subNameValidator = () => {
+    if(subsData.some(item => item.name === newEditedSubData.name)){
+      return Promise.reject(new Error("Ya existe una filial con ese nombre"))
+    }
+    return Promise.resolve()
+  }
+  const subHelperCantValidator = () => {
+    if((!newEditedSubData.max_helpers)||( 0 >= newEditedSubData.max_helpers)){
+      return Promise.reject(new Error("El número ingresado no es válido"))
+    }
+    //Cambiar 5 por subsData[idSelected].current_helpers
+    if( 5 > newEditedSubData.max_helpers){
+      return Promise.reject(new Error("El límite de ayudantes no puede ser inferior a la cantidad de ayudantes asignados"))
+    }
+    return Promise.resolve()
+  }
+
+  const handleChangeInputName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewEditedSubData(prevNewData => ({
+      ...prevNewData,
+      name: event.target.value
+    }))
+  }
+  const handleChangeInputCantHelpers = (value: number | null) => {
+    setNewEditedSubData(prevNewData => ({
+      ...prevNewData,
+      max_helpers: value
+    }))
+  }
+  const handleChangeCheckbox = (value: CheckboxChangeEvent) => {
+    setNewEditedSubData(prevNewData => ({
+      ...prevNewData,
+      active: value.target.checked
+    }))
+  }
+
+
+  function CardHeader({ title, buttonName, handleFunction }: PropType) {
+
+    return (
+      <Flex align="center" gap="small">
+        <h3 style={{ marginRight: 'auto', marginBottom: "0" }}>{title}</h3>
+        <Button type='primary' onClick={handleFunction}>{buttonName}</Button>
+      </Flex>
+    )
+  }
+
+  function LocationMarker() {
+    useMapEvents({
+      click(e) {
+        const x:string = e.latlng.lat.toString()
+        const y:string = e.latlng.lng.toString()
+        setLocationSelected(e.latlng) 
+        setNewSubData(prevNewData => ({
+          ...prevNewData,
+          x_coordinate: x,
+          y_coordinate: y
+        }))
+      },
+    })
+
+    return !locationSelected ? null : (
+      <Marker position={locationSelected}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  }
+
   return (
     <>
-      <p>Administración de filiales</p>
+      <Row>
+        <Col span={24}>
+          <Card 
+            title={<CardHeader 
+              title="Mapa"
+              buttonName="Agregar filial"
+              handleFunction={handleAddingModal}
+              />} 
+            style={{ width: '100%', height: '500px' }}>
+            <MapContainer
+              center={[-35, -58]}
+              zoom={12}
+              zoomControl={false}
+              style={{ borderRadius: '5px', height: '400px' }}
+            >
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              {subsData.map((marcador, index) => (
+                <Marker
+                  key={index}
+                  position={[
+                    parseFloat(marcador.x_coordinate),
+                    parseFloat(marcador.y_coordinate),
+                  ]}
+                  eventHandlers={{
+                    click: () => handleMarkerClick(index),
+                  }}
+                >
+                  <Popup>{marcador.name}</Popup>
+                </Marker>
+              ))}
+            </MapContainer>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          {idSelected > 0 ? (<Card title={<CardHeader 
+            title="Información de la filial"
+            buttonName="Editar"
+            handleFunction={handleEditingModal}
+          />}
+            style={{marginBottom: "30px", marginTop: "10px"}}
+          >
+            <Descriptions 
+              bordered
+              layout="horizontal"
+              column={1}
+              items={items}
+              labelStyle={{ width: '35%' }}
+            />
+          </Card>) : null}
+        </Col>
+      </Row>
+      <Modal
+        title="Agregando una filial"
+        open={isAddingModalOpen}
+        onOk={handleOkAdding}
+        onCancel={handleCancelAdding}
+        cancelText="Cancelar"
+        okText="Confirmar"
+      >
+        <p>Seleccione un lugar en el mapa</p>
+        <div>
+          <MapContainer
+            center={[-35, -58]}
+            zoom={12}
+            zoomControl={false}
+            style={{ borderRadius: '5px', height: '200px' }}
+            
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            <LocationMarker />
+            
+          </MapContainer>
+        </div>
+        <Form
+          layout="vertical"
+        >
+          <Form.Item
+            label="Nombre"
+            required={false}
+            rules={[{required: true, message:"El nombre es obligatorio"},
+                    {validator: subNameValidator}
+            ]}
+          >
+            <Input value={newEditedSubData.name} onChange={(event) => handleChangeInputName(event)}></Input>
+          </Form.Item>
+          <Form.Item
+            label="Cantidad de ayudantes máximo"
+            rules={[{required: true, message:"Este campo es obligatorio"},
+                    {validator: subHelperCantValidator}
+            ]}
+          >
+            <InputNumber value={newEditedSubData.max_helpers} onChange={(value) => handleChangeInputCantHelpers(value)}></InputNumber>
+          </Form.Item>
+          <Form.Item>
+            <Checkbox defaultChecked={newEditedSubData.active} onChange={(value) => handleChangeCheckbox(value)}>Activa</Checkbox>
+          </Form.Item>
+          <p>x: {newSubData.x_coordinate} y:{newSubData.y_coordinate}</p>
+        </Form>
+      </Modal>
+      <Modal
+        title="Editando una filial"
+        open={isEditingModalOpen}
+        onOk={handleOkEditing}
+        onCancel={handleCancelEditing}
+        cancelText="Cancelar"
+        okText="Aplicar cambios"
+      >
+        <Form
+          layout="vertical"
+        >
+          <Form.Item
+            label="Nombre"
+            required={false}
+            rules={[{required: true, message:"El nombre es obligatorio"},
+                    {validator: subNameValidator}
+            ]}
+          >
+            <Input value={newEditedSubData.name} onChange={(event) => handleChangeInputName(event)}></Input>
+          </Form.Item>
+          <Form.Item
+            label="Cantidad de ayudantes máximo"
+            rules={[{required: true, message:"Este campo es obligatorio"},
+                    {validator: subHelperCantValidator}
+            ]}
+          >
+            <InputNumber value={newEditedSubData.max_helpers} onChange={(value) => handleChangeInputCantHelpers(value)}></InputNumber>
+          </Form.Item>
+          <Form.Item>
+            <Checkbox defaultChecked={newEditedSubData.active} onChange={(value) => handleChangeCheckbox(value)}>Activa</Checkbox>
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   )
 }
