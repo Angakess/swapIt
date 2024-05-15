c39079e14516139b0d5a8bd14a7594c07c689f84
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-14 21:08:25 -0300

    remove PostsLayout

--- client/src/features/posts/pages/Posts.tsx
+++ client/src/features/posts/pages/Posts.tsx
@@ -1,9 +1,69 @@
-import { PostsLayout } from '@Posts/layout'
+import { useEffect, useState } from 'react'
+import { Divider } from 'antd'
+
+import { useAuth } from '@Common/hooks'
+import { PostsList, SearchAndFilter } from '@Posts/components'
+import {
+  PostModel,
+  getCategoryList,
+  getPostsListsExchanger,
+} from '@Posts/helpers/getPostsListsExchanger'
+
+type SelectOption = {
+  label: string
+  value: string
+}
+
+async function mapCategoiresToSelectOptions(): Promise<SelectOption[]> {
+  const categories = await getCategoryList()
+  return categories.map(({ name }) => ({ label: name, value: name }))
+}
 
 export function Posts() {
+  const { user } = useAuth()
+
+  const [categoriesOptions, setCategoriesFilter] = useState<SelectOption[]>([
+    { label: 'Todas las categorías', value: '' },
+  ])
+
+  const [posts, setPosts] = useState<PostModel[]>([])
+
+  useEffect(() => {
+    ;(async () => {
+      const categories = await mapCategoiresToSelectOptions()
+      setCategoriesFilter([...categoriesOptions, ...categories])
+    })()
+    ;(async () => {
+      const p = await getPostsListsExchanger({ excludeUserId: user!.id })
+      setPosts(p)
+    })()
+  }, [])
+
   return (
-    <PostsLayout
-      header={<h2 style={{ marginBottom: '2rem' }}>Publicaciones</h2>}
-    />
+    <>
+      <h2 style={{ marginBottom: '2rem' }}>Publicaciones</h2>
+      <SearchAndFilter
+        searchBar={{ placeholder: 'Busca un producto' }}
+        filters={[
+          {
+            placeholder: 'Categoría',
+            options: categoriesOptions,
+            defaultValue: '',
+          },
+          {
+            placeholder: 'Estado',
+            options: [
+              { label: 'Todos los estados', value: '' },
+              { label: 'Nuevo', value: 'NUEVO' },
+              { label: 'Usado', value: 'USADO' },
+              { label: 'Defectuoso', value: 'DEFECTUOSO' },
+            ],
+            defaultValue: '',
+          },
+        ]}
+      />
+      <Divider />
+      <PostsList posts={posts} />
+    </>
   )
 }
