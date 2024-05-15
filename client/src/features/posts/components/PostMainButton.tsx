c768542b2614c106539a8dde1308eaa1d208d889
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-05-15 07:57:08 -0300

    armando los botones

--- client/src/features/posts/components/PostMainButton.tsx
+++ client/src/features/posts/components/PostMainButton.tsx
@@ -1,14 +1,65 @@
-import { Button } from 'antd'
+import { Button, Dropdown, Space } from 'antd'
+import { DownOutlined } from '@ant-design/icons'
 
-export function PostMainButton() {
+import { useAuth } from '@Common/hooks'
+import { PostModel } from '@Posts/helpers/getPostsListsExchanger'
+
+export function PostMainButton({ post }: { post: PostModel }) {
+  const { user } = useAuth()
+
+  if (user!.role === 'EXCHANGER') {
+    if (user!.id === post.user.id) {
+      return (
+        <Button
+          type="primary"
+          block
+          size="large"
+          style={{ fontWeight: '700', marginBottom: '1.5rem' }}
+        >
+          Editar
+        </Button>
+      )
+    }
+
+    return (
+      <Button
+        type="primary"
+        block
+        size="large"
+        style={{ fontWeight: '700', marginBottom: '1.5rem' }}
+      >
+        Intercambiar
+      </Button>
+    )
+  }
+
+  if (post.state.name === 'activo') {
+    return (
+      <Button
+        type="primary"
+        block
+        size="large"
+        style={{ fontWeight: '700', marginBottom: '1.5rem' }}
+        disabled
+      >
+        Publicación activa
+      </Button>
+    )
+  }
+
   return (
-    <Button
-      type="primary"
-      block
-      size="large"
-      style={{ fontWeight: '700', marginBottom: '1.5rem' }}
-    >
-      Intercambiar
-    </Button>
+    <>
+      <Dropdown menu={{}}>
+        <Button>
+          <Space>
+            Button
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
+      <Dropdown.Button menu={{}} danger>
+        Danger
+      </Dropdown.Button>
+    </>
   )
 }
