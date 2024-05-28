a0b942871223d532f1f9f2407de8471ced3e973e
andres <andresgabriel.kessler@gmail.com>
2024-05-27 21:40:14 -0300

    filtro de fecha en el listado de turnos

--- client/src/features/turns/functions/tableColumnSearchProps.tsx
+++ client/src/features/turns/functions/tableColumnSearchProps.tsx
@@ -1,9 +1,11 @@
-import { Button, Input, InputRef, Space, TableColumnType } from 'antd'
+import { Button, DatePicker, Input, InputRef, Space, TableColumnType } from 'antd'
 import { SearchOutlined } from '@ant-design/icons'
 import {
   FilterConfirmProps,
   FilterDropdownProps,
 } from 'antd/es/table/interface'
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 type DataIndex = keyof DataType
 interface DataType {
@@ -13,6 +15,8 @@ interface DataType {
   confirmed: boolean
   [key: string]: string | number | boolean
 }
+
+dayjs.extend(customParseFormat);
 
 export function tableColumnSearchProps(
   dataIndex: DataIndex,
@@ -35,8 +39,23 @@ export function tableColumnSearchProps(
     clearFilters,
     close,
   }: FilterDropdownProps) {
+
+    const dateValue = selectedKeys[0] && typeof selectedKeys[0] === 'string' ? dayjs(selectedKeys[0], "DD/MM/YYYY") : null;
+    
     return (
       <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        {dataIndex === "date" ? 
+        <DatePicker
+            format={"DD-MM-YYYY"}
+            placeholder='Seleccione una fecha'
+            value={dateValue}
+            onChange={(date) => {
+                setSelectedKeys(date ? [date.format("DD/MM/YYYY")] : []);
+            }}
+            allowClear={true}
+            style={{ marginBottom: 8, display: 'block' }}
+        /> : 
+        
         <Input
           ref={searchInput}
           value={selectedKeys[0]}
@@ -47,7 +66,9 @@ export function tableColumnSearchProps(
             handleSearch(selectedKeys as string[], confirm, dataIndex)
           }
           style={{ marginBottom: 8, display: 'block' }}
-        />
+        />}
+        
+
         <Space>
           <Button
             type="primary"
