6f959ec36c66b215a71e944907a4a4dcbdd30fc0
andres <andresgabriel.kessler@gmail.com>
2024-05-29 15:33:49 -0300

    preparacion de SwapsList

--- client/src/features/swaps/pages/SwapsList.tsx
+++ client/src/features/swaps/pages/SwapsList.tsx
@@ -1,7 +1,165 @@
+import { GetProp, InputRef, Space } from "antd"
+import { ColumnsType, TablePaginationConfig } from "antd/es/table"
+import { FilterDropdownProps } from "antd/es/table/interface"
+import { Table, TableProps } from "antd/lib"
+import { useRef, useState } from "react"
+import { tableColumnSearchProps } from "@Common/functions/tableColumnSearchProps"
+
+type DataIndex = keyof DataType
+interface DataType {
+  id: number
+  date: string
+  subsidiary: string
+  confirmed: boolean
+  [key: string]: string | number | boolean
+}
+interface TableParams {
+  pagination?: TablePaginationConfig
+  sortField?: string
+  sortOrder?: string
+  filters?: Parameters<GetProp<TableProps, 'onChange'>>[1]
+}
+
 export function SwapsList() {
+
+    const [data, setData] = useState<DataType[]>([])
+    const [searchText, setSearchText] = useState<DataType>({
+        id: 0,
+        date: '',
+        subsidiary: '',
+        confirmed: false,
+      })
+      const [loading, setLoading] = useState(false)
+
+    const [tableParams, setTableParams] = useState<TableParams>({
+        pagination: {
+          current: 1,
+          pageSize: 10,
+        },
+      })
+      const searchInput = useRef<InputRef>(null)
+
+    const handleTableChange: TableProps['onChange'] = (
+        pagination,
+        filters,
+        sorter
+      ) => {
+        setTableParams({
+          pagination,
+          filters,
+          ...sorter,
+        })
+    
+        if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+          setData([])
+        }
+    
+        const filteredData = data.filter((item: DataType) =>
+          Object.keys(filters).every((key: string) => {
+            if (filters[key]?.length === 0) return true
+            return filters[key]?.includes(item[key])
+          })
+        )
+    
+        setTableParams({
+          ...tableParams,
+          pagination: {
+            ...pagination,
+            total: filteredData.length,
+          },
+        })
+      }
+
+      const handleSearch = (
+        selectedKeys: string[],
+        confirm: FilterDropdownProps['confirm'],
+        dataIndex: DataIndex
+      ) => {
+        confirm(),
+          setSearchText((prevText) => {
+            return {
+              ...prevText,
+              [dataIndex]: selectedKeys[0],
+            }
+          })
+      }
+    
+      const handleReset = (
+        clearFilters: () => void,
+        confirm: FilterDropdownProps['confirm'],
+        dataIndex: DataIndex
+      ) => {
+        clearFilters()
+        setSearchText((searchText) => {
+          return {
+            ...searchText,
+            [dataIndex]: '',
+          }
+        })
+        confirm()
+      }
+
+    const columns: ColumnsType<DataType> = [
+        {
+          title: `ID: ${searchText.id ? searchText.id : ''}`,
+          dataIndex: 'id',
+          render: (id) => `${id}`,
+          ...tableColumnSearchProps('id', handleSearch, handleReset, searchInput),
+        },
+        {
+          title: `Fecha: ${searchText.date ? searchText.date : ''}`,
+          dataIndex: 'date',
+          render: (date) => `${date}`,
+          ...tableColumnSearchProps('date', handleSearch, handleReset, searchInput),
+    
+          //arreglar cuando este conectado al backend
+          sorter: (a, b) => a.date.localeCompare(b.date),
+          defaultSortOrder: 'ascend',
+        },
+        {
+          title: `Filial: ${searchText.subsidiary}`,
+          dataIndex: 'subsidiary',
+          render: (sub) => `${sub}`,
+          ...tableColumnSearchProps(
+            'subsidiary',
+            handleSearch,
+            handleReset,
+            searchInput
+          ),
+          sorter: (a, b) => a.subsidiary.localeCompare(b.subsidiary),
+        },
+        {
+          title: `Estado:`,
+          dataIndex: 'confirmed',
+          render: (confirmed) => (confirmed ? 'Confirmado' : 'Sin confirmar'),
+          filters: [
+            { text: 'Confirmado', value: true },
+            { text: 'Sin confirmar', value: false },
+          ],
+          onFilter: (value, record) => record.confirmed === value,
+          filterSearch: false,
+        },
+        {
+          title: 'Acciones',
+          render: (_: any, record: DataType) => (
+            <Space>
+              
+            </Space>
+          ),
+        },
+      ]
+
     return (
         <>
-        lista de swaps
+            <Table
+            columns={columns}
+            rowKey={(record) => record.id}
+            dataSource={data}
+            pagination={tableParams.pagination}
+            loading={loading}
+            onChange={handleTableChange}
+            locale={{ emptyText: 'No hay turnos disponibles' }}
+        />
         </>
     )
 }
