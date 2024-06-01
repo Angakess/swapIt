7f83662a2517089c2a682c3753f71c8da8e8c9f8
Sai <palaciossimon6@gmail.com>
2024-05-31 21:27:14 -0300

    Add requests fixture (review the data) and add to loadData.sh

--- server/loadData.sh
+++ server/loadData.sh
@@ -3,4 +3,6 @@ python manage.py loaddata initial_category.json   \
                           initial_subsidiary.json \
                           initial_stateuser.json  \
                           initial_user.json       \
-                          initial_post.json
+                          initial_post.json \
+                          initial_requeststate.json \
+                            initial_request.json
