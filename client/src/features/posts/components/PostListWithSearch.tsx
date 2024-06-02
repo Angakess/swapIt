fbf25d942c4d61f406e1097011ba945f4c6327ea
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-06-02 02:30:38 -0300

    add PostListWithSearch and use it in PostsExhanger

---
+++ client/src/features/posts/components/PostListWithSearch.tsx
@@ -0,0 +1,91 @@
+import { useCallback, useEffect, useState } from 'react'
+import { SearchAndFilter, SearchAndFilterProps } from './SearchAndFilter'
+import { SelectOption, mapCategoriesToSelectOptions } from '@Posts/helpers'
+import { Divider } from 'antd'
+import { PostsList } from './PostsList'
+import { PostModel, StateModel } from '@Common/api'
+
+export type PostListWithSearchProps = {
+  posts: PostModel[]
+  setPosts: React.Dispatch<React.SetStateAction<PostModel[]>>
+  isLoading: boolean
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+  filterStates: StateModel['name'][]
+  fetchPosts: (values: Record<string, string>) => Promise<PostModel[]>
+  initialFetchPostsStatus: StateModel['name']
+  showStatus?: boolean
+  additionalFilters?: SearchAndFilterProps['filters']
+}
+
+export function PostListWithSearch({
+  posts,
+  setPosts,
+  isLoading,
+  setIsLoading,
+  filterStates,
+  fetchPosts,
+  initialFetchPostsStatus,
+  showStatus = false,
+  additionalFilters = {},
+}: PostListWithSearchProps) {
+  const [categoriesOptions, setCategoriesOptions] = useState<SelectOption[]>([
+    { label: 'Todas las categorías', value: '' },
+  ])
+
+  const updatePosts = useCallback(
+    (newPosts: PostModel[]) => {
+      setPosts(
+        newPosts.filter(({ state }) => filterStates.includes(state.name))
+      )
+    },
+    [filterStates, setPosts]
+  )
+
+  const handleSearch: SearchAndFilterProps['onSearch'] = async (values) => {
+    setIsLoading(true)
+    const newPosts = await fetchPosts(values)
+    updatePosts(newPosts)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    setIsLoading(true)
+    Promise.all([
+      mapCategoriesToSelectOptions('name').then((categories) =>
+        setCategoriesOptions([
+          { label: 'Todas las categorías', value: '' },
+          ...categories,
+        ])
+      ),
+      fetchPosts({ status: initialFetchPostsStatus }).then(updatePosts),
+    ]).finally(() => setIsLoading(false))
+  }, [fetchPosts, initialFetchPostsStatus, setIsLoading, updatePosts])
+
+  return (
+    <>
+      <SearchAndFilter
+        searchPlaceholder="Busca una publicación"
+        disabled={isLoading}
+        onSearch={handleSearch}
+        filters={{
+          category: {
+            initialValue: '',
+            options: categoriesOptions,
+          },
+          state: {
+            initialValue: '',
+            options: [
+              { label: 'Todos los estados de producto', value: '' },
+              { label: 'Nuevo', value: 'NUEVO' },
+              { label: 'Usado', value: 'USADO' },
+              { label: 'Defectuoso', value: 'DEFECTUOSO' },
+            ],
+          },
+          ...additionalFilters,
+        }}
+      />
+      <Divider />
+      <PostsList posts={posts} isLoading={isLoading} showStatus={showStatus} />
+    </>
+  )
+}
