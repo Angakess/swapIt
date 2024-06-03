a21990273413994ff8997eb4d489b806c03c26c9
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-06-03 16:59:40 -0300

    add view protection to <Request />

--- client/src/features/requests/pages/Request.tsx
+++ client/src/features/requests/pages/Request.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Spin } from 'antd'
+
+import { RequestModel, getRequestById } from '@Common/api'
+import { useAuth } from '@Common/hooks'
+import { Page404 } from '@Common/pages'
+
 export function Request() {
-    return (
-        <p>una request</p>
-    )
+  const { id } = useParams()
+  const { user } = useAuth()
+
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [request, setRequest] = useState<RequestModel | null>(null)
+
+  useEffect(() => {
+    ;(async () => {
+      setIsLoading(true)
+      const resp = await getRequestById(parseInt(id!, 10))
+      setRequest(resp)
+      setIsLoading(false)
+    })()
+  }, [id])
+
+  // Si está cargando, mostrar spin de carga:
+  if (isLoading) {
+    return <Spin size="large" style={{ width: '100%' }} />
+  }
+
+  // Si:
+  // - la request no existe
+  // - o el usuario no está involucrado
+  // entonces: 404.
+  if (
+    request === null ||
+    ![request.user_maker, request.user_receive].includes(user!.id)
+  ) {
+    return <Page404 />
+  }
+
+  // Cualquier otro caso, mostrar el componente:
+
+  return <p>una request</p>
 }
