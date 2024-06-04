5f792e6d23e8da5461e42cb22aa67b174022a010
mauri-cj <mauricocella@gmail.com>
2024-06-03 23:13:31 -0300

    Agregado listado de calificaciones hechas a un usuario

--- server/rating/urls.py
+++ server/rating/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # path('ratings/', views.RatingsListView.as_view(), name='ratings-list'),
     # path('ratings/<int:pk>/', views.RatingsDetailView.as_view(), name='ratings-detail'),
     path('ratings/create/', views.RatingCreateView.as_view(), name='ratings-create'),
+    path('ratings/user/<int:user_id>/', views.RatingOfUser.as_view(), name='ratings-user'),
     # path('ratings/update/<int:pk>/', views.RatingsUpdateView.as_view(), name='ratings-update'),
     # path('ratings/delete/<int:pk>/', views.RatingsDeleteView.as_view(), name='ratings-delete'),
 ]
