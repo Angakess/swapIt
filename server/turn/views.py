6c867760022451abb2c42c749528d46bdbeb547e
mauri-cj <mauricocella@gmail.com>
2024-06-04 21:06:55 -0300

    Fixeando algunos TODO

--- server/turn/views.py
+++ server/turn/views.py
@@ -3,9 +3,11 @@ from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from common.email import send_email_to_user
 
 from user.models import UserAccount
 from .models import Turn, TurnState
+from app_post.models import PostState
 from .serializers import (
     TurnHelperListSerializer,
     TurnExchangerListSerializer,
@@ -62,7 +64,6 @@ class ListTurnsTodayView(APIView):
         return Response(serializer.data)
 
 class TurnsValidateView(APIView):
-    #TODO: Enviar a los usuarios un email para que califiquen al otro usuario.
     def post(self, request):
         data = request.data
         id_turn = data.get("id_turn")
@@ -75,10 +76,32 @@ class TurnsValidateView(APIView):
                     "Los códigos no coinciden."], "data": {}},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        turn.state = TurnState.objects.get(name="efectuado")
+        
+        email_maker = turn.user_maker.email
+        name_maker = turn.user_maker.full_name
+        email_received = turn.user_received.email
+        name_received = turn.user_received.full_name
         score_maker = turn.post_maker.value
         score_received = turn.post_receive.value
+        
+        send_email_to_user(
+            [email_maker],
+            "Califica tu intercambio",
+            f"¡Felicitaciones {name_maker}! Has finalizado un intercambio con {name_received}, has sumado {score_maker} puntos. \n" +
+            f"Puedes calificar a {name_received} en el siguiente enlace: http://localhost:5173/user/calificate/ \n" +
+            f"¡Gracias por confiar en SwapIt!"
+        )
 
+        send_email_to_user(
+            [email_received],
+            "Califica tu intercambio",
+            f"¡Felicitaciones {name_received}! Has finalizado un intercambio con {name_maker}, has sumado {score_received} puntos. \n" +
+            f"Puedes calificar a {name_maker} en el siguiente enlace: http://localhost:5173/user/calificate/ \n" +
+            f"¡Gracias por confiar en SwapIt!"
+        )
+
+
+        turn.state = TurnState.objects.get(name="efectuado")
         turn.user_maker.score += score_maker
         turn.user_received.score += score_received
         turn.user_maker.save()
@@ -91,18 +114,31 @@ class TurnsValidateView(APIView):
         )
 
 
-# [{"id_turn":1},{"id_turn":2},{"id_turn":3},{"id_turn":4},{"id_turn":5}]
 
 
 class TurnsRejectView(APIView):
-    # TODO: Actualizar stock
     def post(self, request):
         data = request.data
+        # [{"id_turn":1},{"id_turn":2},{"id_turn":3},{"id_turn":4},{"id_turn":5}]
         ids_turns = [item["id_turn"] for item in data]
         turns = Turn.objects.filter(id__in=ids_turns)
         for turn in turns:
             turn.state = TurnState.objects.get(name="no efectuado")
+            post_maker = turn.post_maker
+            post_receive = turn.post_receive
+
+            if post_maker.stock_product == 0 and post_maker.state.id == 7:
+                post_maker.state = PostState.objects.filter(id=1).first()
+
+            if post_receive.stock_product == 0 and post_receive.state.id == 7:
+                post_receive.state = PostState.objects.filter(id=1).first()
+
+            post_maker.stock_product += 1
+            post_receive.stock_product += 1
+            post_maker.save()
+            post_receive.save()
             turn.save()
+
         return Response(
             {
                 "ok": True,
