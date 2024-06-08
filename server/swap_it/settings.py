70897b0c88892a0f78ffa685beb7f7d04ce16089
andres <andresgabriel.kessler@gmail.com>
2024-06-08 17:25:32 -0300

    cambio de bot para mandar mails y ajustes de fixtures

--- server/swap_it/settings.py
+++ server/swap_it/settings.py
@@ -68,13 +68,13 @@ DATABASES = {
     }
 }
 
-DEFAULT_FROM_EMAIL = 'is2.caritas@hotmail.com'
+DEFAULT_FROM_EMAIL = 'caritas_is2@hotmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
-EMAIL_HOST_PASSWORD = 'lwiuczecuytgogmg'
+EMAIL_HOST_PASSWORD = 'nhrzbkeodflhkmrc'
 
 
 AUTH_PASSWORD_VALIDATORS = [
