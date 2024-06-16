5a711c64c22f76c885d769e18861de7ab98ab963
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-06-15 22:13:38 -0300

    add stats for individual categories

--- client/src/features/stats/pages/StatsPosts.tsx
+++ client/src/features/stats/pages/StatsPosts.tsx
@@ -1,108 +1,195 @@
 import {
-  PostStateModel,
+  CategoryModel,
+  PostModel,
   PostStateNameEnum,
   PostStateNames,
   getCategoryList,
   getPostList,
 } from '@Common/api'
 import { PageTitle } from '@Common/components'
-import { Column } from '@ant-design/charts'
-import { Button, Card, Flex, Spin, Typography } from 'antd'
+import { Column, Pie } from '@ant-design/charts'
+import { Button, Card, Col, Flex, Row, Spin, Typography } from 'antd'
 import { useEffect, useState } from 'react'
 
-type TempDataStateType = Record<PostStateModel['name'], number>
-type TempDataType = Record<string, TempDataStateType>
-
-type DataType = {
-  category: string
-  state: PostStateNames
-  amount: number
-}
-
-async function loadData() {
-  const [categories, posts] = await Promise.all([
-    getCategoryList(),
-    getPostList(),
-  ])
-  const postStates: PostStateNames[] = Object.values(PostStateNameEnum)
-
-  /** Contiene todos los posts agrupados por categoría y estado */
-  const data: TempDataType = {}
-
-  // Crear la estructura del objecto con todos los valores en 0
-  categories.forEach((category) => {
-    data[category.name] = {} as TempDataStateType
-    postStates.forEach((state) => (data[category.name][state] = 0))
-  })
-
-  // Cargar los posts en la estructura
-  posts.forEach((post) => data[post.category.name][post.state.name]++)
-
-  return data
-}
-
-async function formatData() {
-  return Object.entries(await loadData()).flatMap(([category, states]) =>
-    Object.entries(states).map(([state, amount]) => ({
-      category,
-      state: state as PostStateModel['name'],
-      amount: amount,
-    }))
-  )
-}
-
 export function StatsPosts() {
-  const [postsData, setPostsData] = useState<DataType[]>()
+  const [posts, setPosts] = useState<PostModel[]>([])
+  const [categories, setCategories] = useState<CategoryModel[]>([])
   const [isLoading, setIsLoading] = useState(false)
-  const [stack, setStack] = useState(true)
 
   useEffect(() => {
-    setIsLoading(true)
-    formatData().then((data) => {
-      setPostsData(data)
+    ;(async () => {
+      setIsLoading(true)
+      const [c, p] = await Promise.all([getCategoryList(), getPostList()])
+      setCategories(c)
+      setPosts(p)
       setIsLoading(false)
-    })
+    })()
   }, [])
 
   return (
     <>
       <PageTitle title="Estadísticas de publicaciones" />
-      <Card>
-        <Flex
-          justify="space-between"
-          align="center"
-          style={{ marginBottom: '0.75rem' }}
-        >
-          <Typography.Title level={3} style={{ margin: 0 }}>
-            Cantidad de publicaciones por categoría
-          </Typography.Title>
-          <Button
-            type={stack ? 'primary' : 'default'}
-            size="small"
-            style={{ width: '6.25rem' }}
-            onClick={() => setStack(!stack)}
-          >
-            {stack ? 'Agrupado' : 'Desagrupado'}
-          </Button>
-        </Flex>
+      <StatPostColum
+        isLoading={isLoading}
+        categories={categories}
+        posts={posts}
+      />
+      <Row gutter={[12, 12]}>
         {isLoading ? (
           <Spin size="large" style={{ width: '100%', margin: '2.5rem 0' }} />
         ) : (
-          <Column
-            loading={isLoading}
-            data={postsData}
-            xField="category"
-            yField="amount"
-            height={500}
-            seriesField={stack ? [] : 'state'}
-            stack={{
-              groupBy: ['x', 'series'],
-              series: true,
-            }}
-            colorField="state"
-          />
+          categories.map((category) => (
+            <Col md={12}>
+              <CategoryPie
+                key={category.name}
+                category={category.name}
+                posts={posts}
+                isLoading={isLoading}
+              />
+            </Col>
+          ))
         )}
-      </Card>
+      </Row>
     </>
+  )
+}
+
+type StatPostColumProps = {
+  isLoading: boolean
+  posts: PostModel[]
+  categories: CategoryModel[]
+}
+
+function StatPostColum({ isLoading, posts, categories }: StatPostColumProps) {
+  type DataType = {
+    category: string
+    state: PostStateNames
+    amount: number
+  }
+
+  const [stack, setStack] = useState(true)
+  const [data, setData] = useState<DataType[]>([])
+
+  useEffect(() => {
+    const states: PostStateNames[] = Object.values(PostStateNameEnum)
+    setData(
+      categories.flatMap(({ name }) => {
+        const categoryPosts = posts.filter((p) => p.category.name === name)
+        return states.map((state) => ({
+          category: name,
+          state,
+          amount: categoryPosts.filter((p) => p.state.name === state).length,
+        }))
+      })
+    )
+  }, [categories, posts])
+
+  return (
+    <Card style={{ marginBottom: '12px' }}>
+      <Flex
+        justify="space-between"
+        align="center"
+        style={{ marginBottom: '0.75rem' }}
+      >
+        <Typography.Title level={3} style={{ margin: 0 }}>
+          Cantidad de publicaciones por categoría
+        </Typography.Title>
+        <Button
+          type={stack ? 'primary' : 'default'}
+          size="small"
+          style={{ width: '6.25rem' }}
+          onClick={() => setStack(!stack)}
+          disabled={isLoading}
+        >
+          {stack ? 'Agrupado' : 'Desagrupado'}
+        </Button>
+      </Flex>
+      {isLoading ? (
+        <Spin size="large" style={{ width: '100%', margin: '2.5rem 0' }} />
+      ) : (
+        <Column
+          loading={isLoading}
+          data={data}
+          xField="category"
+          yField="amount"
+          height={500}
+          seriesField={stack ? [] : 'state'}
+          stack={{
+            groupBy: ['x', 'series'],
+            series: true,
+          }}
+          colorField="state"
+          scale={{ color: { palette: 'category10' } }}
+        />
+      )}
+    </Card>
+  )
+}
+
+type CategoryPieProps = {
+  isLoading: boolean
+  category: string
+  posts: PostModel[]
+}
+
+function CategoryPie({ isLoading, category, posts }: CategoryPieProps) {
+  type DataType = {
+    state: PostStateNames
+    amount: number
+  }
+
+  const [data, setData] = useState<DataType[]>([])
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    const states: PostStateNames[] = Object.values(PostStateNameEnum)
+    const newData = states.map((state) => ({
+      state,
+      amount: posts.filter(
+        (p) => p.state.name === state && p.category.name === category
+      ).length,
+    }))
+
+    setData(newData)
+    setTotal(newData.reduce((acc, { amount }) => acc + amount, 0))
+  }, [category, posts])
+
+  console.log(data)
+
+  return (
+    <Card style={{ textTransform: 'capitalize' }}>
+      <Typography.Title level={4} style={{ margin: 0 }}>
+        {category}
+      </Typography.Title>
+      {isLoading ? (
+        <Spin size="large" style={{ width: '100%', margin: '2.5rem 0' }} />
+      ) : (
+        <Pie
+          loading={isLoading}
+          data={data}
+          angleField="amount"
+          colorField="state"
+          legend={true}
+          height={400}
+          innerRadius={0.6}
+          scale={{ color: { palette: 'category10' } }}
+          style={{
+            stroke: '#fff',
+            inset: 1,
+            radius: 10,
+          }}
+          interaction={{
+            tooltip: {
+              render: (e, { title, items }) => {
+                const name = items[0].name
+                const amount =
+                  data.find(({ state }) => state === name)?.amount ?? 0
+                return `<b>${name}:</b> ${amount} <i>(${((amount * 100) / total).toFixed(2)}%)</i>`
+              },
+            },
+          }}
+        />
+      )}
+    </Card>
   )
 }
