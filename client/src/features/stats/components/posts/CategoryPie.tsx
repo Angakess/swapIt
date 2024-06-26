f1e12f998122844adf1377f25666df5443d51381
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-06-26 19:54:44 -0300

    add empty message in category pie

--- client/src/features/stats/components/posts/CategoryPie.tsx
+++ client/src/features/stats/components/posts/CategoryPie.tsx
@@ -10,12 +10,12 @@ type CategoryPieProps = {
   posts: PostModel[]
 }
 
-export function CategoryPie({ isLoading, category, posts }: CategoryPieProps) {
-  type DataType = {
-    state: PostStateNames
-    amount: number
-  }
+type DataType = {
+  state: PostStateNames
+  amount: number
+}
 
+export function CategoryPie({ isLoading, category, posts }: CategoryPieProps) {
   const [data, setData] = useState<DataType[]>([])
   const [total, setTotal] = useState(0)
 
@@ -35,39 +35,64 @@ export function CategoryPie({ isLoading, category, posts }: CategoryPieProps) {
   }, [category, isLoading, posts])
 
   return (
-    <Card style={{ textTransform: 'capitalize' }}>
+    <Card style={{ textTransform: 'capitalize', height: '100%' }}>
       <Typography.Title level={4} style={{ margin: 0 }}>
         {category}
       </Typography.Title>
-      {isLoading || data.length === 0 ? (
-        <Spin size="large" style={{ width: '100%', margin: '2.5rem 0' }} />
-      ) : (
-        <Pie
-          loading={isLoading || data.length === 0}
-          data={data}
-          angleField="amount"
-          colorField="state"
-          legend={true}
-          height={400}
-          innerRadius={0.6}
-          scale={{ color: { palette: 'category10' } }}
-          style={{
-            stroke: '#fff',
-            inset: 1,
-            radius: 10,
-          }}
-          interaction={{
-            tooltip: {
-              //@ts-expect-error _ y items son de tipo any
-              render: (_, { items }) => {
-                const amount = items[0].value
-                const color = items[0].color
-                return `<span style="color:${color}">&#x25A0;</span> <b>Cantidad:</b> ${amount} <i>(${((amount * 100) / total).toFixed(2)}%)</i>`
-              },
-            },
-          }}
-        />
-      )}
+      <PieContent data={data} isLoading={isLoading} total={total} />
     </Card>
+  )
+}
+
+type PieContentProps = {
+  isLoading: boolean
+  data: DataType[]
+  total: number
+}
+
+function PieContent({ isLoading, data, total }: PieContentProps) {
+  if (isLoading || data.length === 0) {
+    return <Spin size="large" style={{ width: '100%', margin: '2.5rem 0' }} />
+  }
+
+  if (total === 0) {
+    return (
+      <Typography.Paragraph
+        style={{ textTransform: 'none', marginTop: '0.75rem' }}
+      >
+        No hay publicaciones con esta categoría
+      </Typography.Paragraph>
+    )
+  }
+
+  return (
+    <Pie
+      loading={isLoading || data.length === 0}
+      data={data}
+      angleField="amount"
+      colorField="state"
+      legend={true}
+      height={400}
+      innerRadius={0.6}
+      scale={{ color: { palette: 'category10' } }}
+      style={{
+        stroke: '#fff',
+        inset: 1,
+        radius: 10,
+      }}
+      interaction={{
+        tooltip: {
+          //@ts-expect-error _ y items son de tipo any
+          render: (_, { items }) => {
+            const amount = items[0].value
+            const color = items[0].color
+
+            console.log(items)
+
+            return `<span style="color:${color}">&#x25A0;</span> <b>Cantidad:</b> ${amount} <i>(${((amount * 100) / total).toFixed(2)}%)</i>`
+          },
+        },
+      }}
+    />
   )
 }
