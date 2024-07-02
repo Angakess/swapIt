3727709d4fb9c011eb83a0900b5eed31b475d1b7
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-07-02 20:08:52 -0300

    fixture loadAdminOnly

---
+++ server/loadAdminOnly.sh
@@ -0,0 +1,6 @@
+python manage.py loaddata \
+                          initial_poststate.json    \
+                          initial_stateuser.json    \
+                          initial_admin.json        \
+                          initial_requeststate.json \
+                          initial_turnstate.json    \
