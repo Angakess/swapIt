1077c87acc56a50feb3a511c5cd786edf32c85e7
andres <andresgabriel.kessler@gmail.com>
2024-07-02 22:43:37 -0300

    fix de ingreso de donacion

--- client/src/features/donation/components/AmountChoice.tsx
+++ client/src/features/donation/components/AmountChoice.tsx
@@ -1,8 +1,8 @@
-import { Button, Card, Flex, InputNumber } from 'antd'
+import { Button, Card, Flex, Input, InputNumber } from 'antd'
 import { useState } from 'react'
 
 interface ErrorType {
-  status: "" | "warning" | "error" | undefined
+  status: '' | 'warning' | 'error' | undefined
   msg: string
 }
 
@@ -15,28 +15,47 @@ export function AmountChoice({
   setAmount: (x: number | null) => void
   setShowForm: (x: boolean) => void
 }) {
-
   const [disabledButton, setDisableButton] = useState(false)
-  const [error, setError] = useState<ErrorType>({status:"", msg:""})
+  const [error, setError] = useState<ErrorType>({ status: '', msg: '' })
 
-  const onInputChange = (value: number | null) => {
+  const onInputChange = (value: React.ChangeEvent<HTMLInputElement>) => {
     console.log(value)
-    if (value) {
-      if(value <= 0){
+    const v = value.target.value
+    /* const sign = v.charAt(0)
+    if(sign === "-"){
+      const negNumber = v.slice(1)
+      if(!/^\d*$/.test(negNumber)){
         setDisableButton(true)
-        setError({status: "error", msg: "El monto debe ser mayor a 0"})
-        setAmount(value)
+        setError({ status: 'error', msg: 'El monto debe ser positivo' })
         return
       }
-      setDisableButton(false)
-      setError({status: "", msg: ""})
-      setAmount(value)
       return
-    }else {
+    } */
+
+    if (!/^\d*$/.test(v)) {
       setDisableButton(true)
-      setError({status: "error", msg: "Ingrese un monto"})
-      setAmount(null)
+      setError({ status: 'error', msg: 'Debe ingresar un número positivo' })
+
+      return
     }
+    if (v == '') {
+      setDisableButton(true)
+      setError({ status: 'error', msg: 'Ingrese un monto' })
+      setAmount(null)
+      return
+    }
+    const vNumber = Number(v)
+
+    if (vNumber <= 0) {
+      setDisableButton(true)
+      setError({ status: 'error', msg: 'El monto debe ser positivo' })
+      setAmount(vNumber)
+      return
+    }
+    setDisableButton(false)
+    setError({ status: '', msg: '' })
+    setAmount(vNumber)
+    return
   }
 
   return (
@@ -50,19 +69,29 @@ export function AmountChoice({
         styles={{ header: { paddingLeft: '18px' } }}
       >
         <Flex vertical justify="start" align="start" gap="small">
-          <p style={{fontSize: "16px", padding:"7px"}}>Ingrese un monto en pesos (ARS)</p>  
-          <InputNumber
-            defaultValue={100}
+          <p style={{ fontSize: '16px', padding: '7px' }}>
+            Ingrese un monto en pesos (ARS)
+          </p>
+          <Input
+            /* defaultValue={100} */
             status={error.status}
             onChange={(e) => onInputChange(e)}
             size="large"
             /* min={1} */
             placeholder="Ingrese un monto"
             style={{ width: '100%' }}
-            value={amount}
+            /* value={amount?.toString()} */
           />
-          <p style={{color:"#FF4D4F"}}>{error.msg}</p>
-          <Button size='large' type='primary' style={{marginLeft:"auto", marginTop:"10px"}} disabled={disabledButton} onClick={() => setShowForm(true)}>Confirmar</Button>
+          <p style={{ color: '#FF4D4F' }}>{error.msg}</p>
+          <Button
+            size="large"
+            type="primary"
+            style={{ marginLeft: 'auto', marginTop: '10px' }}
+            disabled={disabledButton}
+            onClick={() => setShowForm(true)}
+          >
+            Confirmar
+          </Button>
         </Flex>
         {/* <Button size='large' type='primary' style={{display:"flex",marginLeft:"auto", marginTop: "24px"}}>Confirmar</Button> */}
       </Card>
